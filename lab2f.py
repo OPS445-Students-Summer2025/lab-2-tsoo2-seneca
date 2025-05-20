d9bef5c4792ee09efc390a1814348558dc66be61
Thit Sann Oo <58143685+tsoo2-seneca@users.noreply.github.com>
2025-05-20 17:35:26 -0400

    tsoo2_lab2_submission

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Thit Sann Oo
+# Author ID: 170547236
+# Date Created: 2025/05/20
+
+import sys
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
+
